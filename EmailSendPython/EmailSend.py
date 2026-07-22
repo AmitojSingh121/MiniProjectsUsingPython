3633e5c1300e7b2e5730fcbb03182f72b5963c80
AmitojSingh <amitojs645@gmail.com>
2026-07-22 14:19:41 +0530

    Mini Project Using Python Developed By Amitoj Singh

--- EmailSendPython/EmailSend.py
+++ EmailSendPython/EmailSend.py
@@ -2,9 +2,14 @@
 import smtplib as s 
 # we create a variable whose name is Obj
 Obj = s.SMTP('smtp.gmail.com',587)
-Obj.ehlo()
+# this is a server
+# ehlo which identifies the Script to the mail server
+Obj.ehlo() 
+# this is a server also
+# starttls Unsecured connection nuh secured connection ch convert kar reha ha 
 Obj.starttls()
 Obj.login("amitojsingh861@gmail.com","wyav ljdh jckz wctv")
+# Email Id       Password Generate 
 subject = "test Python"
 body = "I Love Python"
 massage = "subject:{}\n\n{}".format(subject,body) 
